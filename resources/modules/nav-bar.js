4ecc17d996cf85b8cc2ad289b3c461d61968cd35
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-09-10 11:11:19 +0100

    Functions added for nav buttons
    Relates #8
    
    Co-authored-by: Terrence Chung <netceer112@gmail.com>

--- resources/modules/nav-bar.js
+++ resources/modules/nav-bar.js
@@ -1,13 +1,17 @@
 import h from "./modules/create-element.js";
 
 const navBar = () => {
-  const searchButton = h("button", {}, "search");
+  const searchButton = h(
+    "button",
+    { onclick: search(event.target.parentElement.value) },
+    "search"
+  );
 
-  const randomButton = h("button", {}, "random gossip");
-  const allFactsButton = h("button", {}, "all the gossip");
+  const randomButton = h("button", { onclick: randomFact() }, "random gossip");
+  const allFactsButton = h("button", { onclick: allFacts() }, "all the gossip");
   const searchInput = h("input", {}, "which bitch?", searchButton);
-  const logoutButon = h("button", {onclick: logout()}, "logout");
-  const addFactButton = h("button", {}, "+");
+  const logoutButton = h("button", { onclick: logout() }, "logout");
+  const addFactButton = h("button", { onclick: addFact() }, "+");
 
   return h(
     "nav",
@@ -16,15 +20,47 @@ const navBar = () => {
     allFactsButton,
     searchInput,
     searchButton,
-    logoutButon,
+    logoutButton,
     addFactButton
   );
 };
 
+const fetchHelper = (url) => {
+  return fetch(`https://fun-facs-api.herokuapp.com/${url}`)
+    .then((response) => {
+      if (!response.ok) {
+        const error = new Error("HTTP Error");
+        error.status = response.status;
+        throw error;
+      } else {
+        return response.json();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      // Draw a message to page to inform user of error
+    });
+};
+//----- Button functions -----
+const search = (name) => {
+  const factsArray = fetchHelper(`facts/name/${name}`);
+  // Populate page with factsArray
+};
+
+const randomFact = () => {
+  const fact = fetchHelper(`facts/random`);
+  // Populate page with fact
+};
+
+const allFacts = () => {
+  const factsArray = fetchHelper("facts/");
+  // Populate page with factsArray
+};
+
 const logout = () => {
-    window.localStorage.removeItem("access_token")
-    // remove HTML
-    // load landing page
-}
+  window.localStorage.removeItem("access_token");
+  // remove HTML
+  // load landing page
+};
 
 export default navBar;
