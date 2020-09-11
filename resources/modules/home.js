ca9342b9d1481473780416f8da79c302366d57a2
Terrence <netceer112@gmail.com>
2020-09-11 13:25:44 +0100

    feat: add hover effect and home function to book image

--- resources/modules/home.js
+++ resources/modules/home.js
@@ -1,11 +1,16 @@
 import h from "./create-element.js";
 import signUpForm from "./sign-up-form.js";
 import loginForm from "./login-form.js";
+import checkMain from "./check-main.js";
 
 const home = () => {
   const mainEl = h("main", {});
 
-  const imageEl = h("img", { src: "./resources/images/burn-book.png", alt: "Burn book image" });
+  const imageEl = h("img", { src: "./resources/images/burn-book.png", alt: "Burn book image",class:"book-image", onclick: () => {
+    const body = document.querySelector("body");
+    body.innerHTML= "";
+    body.append(home())
+  }});
 
   const signUpEl = h(
     "button",
